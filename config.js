99acc331090cbe4f08435c124fce4c947107af7d
TranHieuTrong <86173756+TranHieuTrong@users.noreply.github.com>
2021-11-07 17:20:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em người yêu của tui ơi !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Em người yêu Tii của tui ơi, em ơi !',
+    introDesc: `Tuôi yêu Tii lắm đó
+    Mà sao Tii hok bít dị hẽ
+    Bít lè iu lém hok
+    Tii dethw cute phô mai que mèo méo meo mèo meo`,
+    btnIntro: 'awww',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu ank quáaaa <33',
+    btnNo: 'Không, Anh yêu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Em lỡ yêu Anh rồi <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
-    messDesc: 'mai mình gặp nhao nhé bae.',
+    btnReply: 'Gửi cho anh nha bé Tii <3',
+    reply: 'Yêu thì yêu mà không yêu thì cũng yêu ank nha Tii <33333333',
+    mess: 'Anh biết mà 🥰. Yêu Em nhiều lắm á Tii 😘😘',
+    messDesc: 'đi học mìn gặp nhao gòi baeee.',
     btnAccept: 'Dạ yêu yêu <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100052097501614' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100074695952284' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
